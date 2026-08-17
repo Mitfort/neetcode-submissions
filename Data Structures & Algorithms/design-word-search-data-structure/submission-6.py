351d8acb829aba18579ab3ba4cc73ed54b7c1732
Mateusz Poszelężny <110680320+Mitfort@users.noreply.github.com>
2026-08-17 16:23:17 +0200

    Add: design-word-search-data-structure - submission-6

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-6.py
@@ -0,0 +1,42 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.dic = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self.dic 
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        
+        curr.endOfWord = True
+        
+    def search(self, word: str) -> bool:
+        q = deque([(self.dic,0)])
+
+        while q: 
+            for obj in range(len(q)):
+                curr,idx = q.popleft()
+ 
+                if idx == len(word) and curr.endOfWord:
+                    return True
+
+                if idx >= len(word): continue
+
+                if word[idx] in curr.children:
+                    q.append((curr.children[word[idx]], idx+1))
+
+                if word[idx] == '.':
+                    print(len(curr.children))
+                    for child in curr.children:
+                        q.append((curr.children[child],idx+1))
+                
+        return False
+        
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
